a53cb0990eea1813d82b2cc634c4b3c054858eff
nikolanikov09 <58115834+nikolanikov09@users.noreply.github.com>
2019-12-08 12:15:32 +0200

    Add files via upload

---
+++ THE long ADVENTURE/beta/ads.js
@@ -0,0 +1,19 @@
+function ad1() {
+    alert("Free games on hazelgames.c1.biz.")
+    alert("Sorry for the ads :(")
+  }
+
+  function myFunction() {
+    alert("Надяваме се да ви хареса!");
+  }
+
+  function ThankFunction() {
+    alert("Thank you for supporting us!");
+  }
+
+  function myFunctihon() {
+    alert("Hope you like it!");
+  }
+
+
+  
